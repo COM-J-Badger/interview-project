441c249c6ab4f5bb97c494e914750d1563fcf918
COM-J-Badger <JBadger1@sheffield.ac.uk>
2024-12-17 12:48:54 +0000

    added back end tasks 1 and 2

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,45 +1,16 @@
 import { SampleData } from "api/types";
 import axios from 'axios';
+//import e from "express";
 import { useEffect, useState } from "react";
 
 function Data() {
     const [data, setData] = useState<SampleData | undefined>(undefined);
 
-    /**
-     * Gets the data.
-     * Hint: consider using the react-query library that is ready to go!
-     * */
-    // useEffect(() => {
-    //     let mounted = true;
-
-    //     const fetchData = async () => {
-    //         const { data: allData } = await axios.get<SampleData>('/api/task1');
-
-    //         if (mounted) {
-    //             setData(allData)
-    //         }
-    //     }
-
-    //     fetchData();
-
-    //     return () => { mounted = false; }
-    // }, [])
-
-    // if (!data) {
-    //     return 'loading data...';
-    // }
-
-    // return (
-    //     <div className='border p-4'>
-    //         <pre className='text-sm'>{JSON.stringify(data, null, 2)}</pre>
-    //     </div>
-    // )
-
     useEffect(() => {
         let mounted = true;
 
         const fetchData = async () => {
-            const { data: allData } = await axios.get<SampleData>('/api/task1');
+            const { data: allData } = await axios.get<SampleData>('/api/back-end');
 
             if (mounted) {
                 setData(allData)
@@ -51,13 +22,50 @@ function Data() {
         return () => { mounted = false; }
     }, [])
 
+    let tasks: number = 0;
+    let problems: number = 0;
+    let issues: number = 0;
+
+    let high : number = 0;
+    let medium : number = 0;
+    let low : number = 0;
+
+    data?.results.forEach(count)
+
+    function count(item: { type: string, priority: string }) {
+        if (item.type === 'task') {
+            tasks++;
+        } else if (item.type === 'problem') {
+            problems++;
+        } else if (item.type === 'issue') {
+            issues++;
+        }
+
+        if (item.priority === 'high') {
+            high++;
+        }
+        else if (item.priority === 'medium') {
+            medium++;
+        }
+        else if (item.priority === 'low') {
+            low++;
+        }
+    }
+
+
     if (!data) {
         return 'loading data...';
     }
 
     return (
         <div className='border p-4'>
-            <pre className='text-sm'> Task 1: {JSON.stringify(data, null, 2)}</pre>
+            <pre className='text-sm'> Back-End Task 1:</pre>
+            <pre className='text-sm'> Tasks = {tasks}, Problems = {problems}, Issues = {issues}</pre>
+            <pre className='text-sm'> Tasks = {((tasks/500) * 100).toFixed(2)}%, Problems = {((problems/500) * 100).toFixed(2)}%, Issues = {((issues/500) * 100).toFixed(2)}%</pre>
+            <br></br>
+            <pre className='text-sm'> Back-End Task 2:</pre>
+            <pre className='text-sm'> High = {high}, Medium = {medium}, Low = {low}</pre>
+            <pre className='text-sm'> High = {((high/500) * 100).toFixed(2)}%, Medium = {((medium/500) * 100).toFixed(2)}%, Low = {((low/500) * 100).toFixed(2)}%</pre>
         </div>
     )
 
